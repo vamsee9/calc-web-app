4dac628cfc34d9c7d9b84fd4cd021454f9ba4a3d
vamsee9 <4n0n5ur7@protonmail.com>
2021-03-25 12:25:30 +0530

    added calculation
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -13,25 +13,63 @@ class App extends Component {
     };
   }
 
+  reset = () => {
+    this.setState({
+      current: "0",
+      previous: [],
+      nextIsReset: false,
+    });
+  };
+
+  addToCurrent = (symbol) => {
+    if (["/", "*", "-", "+"].indexOf(symbol) > -1) {
+      let { previous } = this.state;
+      previous.push(this.state.current + symbol);
+      this.setState({ previous, nextIsReset: true });
+    } else {
+      if (
+        (this.state.current === "0" && symbol !== ".") ||
+        this.state.nextIsReset
+      ) {
+        this.setState({
+          current: symbol,
+          nextIsReset: false,
+        });
+      } else {
+        this.setState({
+          current: this.state.current + symbol,
+        });
+      }
+    }
+  };
+
+  calculate = (symbol) => {
+    let { current, previous, nextIsReset } = this.state;
+    if (previous.length > 0) {
+      current = eval(String(previous[previous.length - 1] + current));
+      this.setState({ current, previous: [], nextIsReset: true });
+    }
+  };
+
   render() {
     const buttons = [
-      { symbol: "C", cols: 3 },
-      { symbol: "/", cols: 1 },
-      { symbol: "7", cols: 1 },
-      { symbol: "8", cols: 1 },
-      { symbol: "9", cols: 1 },
-      { symbol: "*", cols: 1 },
-      { symbol: "4", cols: 1 },
-      { symbol: "5", cols: 1 },
-      { symbol: "6", cols: 1 },
-      { symbol: "-", cols: 1 },
-      { symbol: "1", cols: 1 },
-      { symbol: "2", cols: 1 },
-      { symbol: "3", cols: 1 },
-      { symbol: "+", cols: 1 },
-      { symbol: "0", cols: 2 },
-      { symbol: ".", cols: 1 },
-      { symbol: "=", cols: 1 },
+      { symbol: "C", cols: 3, action: this.reset },
+      { symbol: "/", cols: 1, action: this.addToCurrent },
+      { symbol: "7", cols: 1, action: this.addToCurrent },
+      { symbol: "8", cols: 1, action: this.addToCurrent },
+      { symbol: "9", cols: 1, action: this.addToCurrent },
+      { symbol: "*", cols: 1, action: this.addToCurrent },
+      { symbol: "4", cols: 1, action: this.addToCurrent },
+      { symbol: "5", cols: 1, action: this.addToCurrent },
+      { symbol: "6", cols: 1, action: this.addToCurrent },
+      { symbol: "-", cols: 1, action: this.addToCurrent },
+      { symbol: "1", cols: 1, action: this.addToCurrent },
+      { symbol: "2", cols: 1, action: this.addToCurrent },
+      { symbol: "3", cols: 1, action: this.addToCurrent },
+      { symbol: "+", cols: 1, action: this.addToCurrent },
+      { symbol: "0", cols: 2, action: this.addToCurrent },
+      { symbol: ".", cols: 1, action: this.addToCurrent },
+      { symbol: "=", cols: 1, action: this.calculate },
     ];
 
     return (
@@ -49,7 +87,7 @@ class App extends Component {
               key={i}
               symbol={btn.symbol}
               cols={btn.cols}
-             
+              action={(symbol) => btn.action(symbol)}
             />
           );
         })}
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
 class Button extends Component {
-  render() {
-    return (
-      <div className={`col-${this.props.cols}`}>
-        <button
-          className="calc-button"
-         
-        >
-          {this.props.symbol}
-        </button>
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div className={`col-${this.props.cols}`}>
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
+            </div>
+        )
+    }
 }
 
-export default Button;
+export default Button
